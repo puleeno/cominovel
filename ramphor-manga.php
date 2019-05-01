6fc119ecacda972e5a5d15d21f1cc81ce110c24d
Puleeno Nguyen <puleeno@gmail.com>
2019-05-01 19:27:35 +0700

    add plugin file

---
+++ ramphor-manga.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Ramphor Manga
+ * Plugin URI: https://puleeno.com
+ * Author: Puleeno Nguyen
+ * Author URI: https://puleeno.com
+ * Version: 1.0
+ * Description: Manga plugins
+ * Tags: manga, cloud, cdn
+ */
+
