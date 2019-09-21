d3f77de84c5b8b557f72289e2e3c49e39595e888
Puleeno Nguyen <puleeno@gmail.com>
2019-09-22 00:30:50 +0700

    Added feature for chapter data

--- includes/admin/meta-boxes/class-cominovel-metabox-chapter-data.php
+++ includes/admin/meta-boxes/class-cominovel-metabox-chapter-data.php
@@ -10,7 +10,9 @@ class Cominovel_Metabox_Chapter_Data {
 	public function __construct() {
 		add_action( 'admin_head', array( $this, 'hide_editor_area' ) );
 		add_action( 'add_meta_boxes', array( $this, 'add_meta_box' ), 5 );
+		add_action( 'save_post', array( $this, 'save_chapter_data' ), 10, 2 );
 		add_filter( 'cominovel_register_post_type_chapter_args', array( $this, 'remove_editor_for_comic' ) );
+		add_filter( 'pre_wp_unique_post_slug', array( $this, 'allow_dupplicate_slug' ), 10, 6 );
 
 		if ( class_exists( Wallery::class ) ) {
 			$this->wallery = new Wallery( new MetaboxFactory() );
@@ -25,18 +27,12 @@ class Cominovel_Metabox_Chapter_Data {
 		}
 		$this->is_chapter_edit = true;
 
-		$allowed_post_types = Cominovel_Post_Types::get_allowed_post_types();
-		if ( isset( $_GET['data_type'] ) && in_array( $_GET['data_type'], $allowed_post_types ) ) {
-			$parent_data_type = $_GET['data_type'];
-		} else {
-			$parent_data_type = array_shift( $allowed_post_types );
-		}
-		$post_type       = get_post_type_object( $parent_data_type );
+		$post_type       = get_post_type_object( Cominovel_Post_Types::check_active_data_type() );
 		$this->post_type = $parent_data_type;
 		add_meta_box(
 			'cominovel_comic_of_chapter',
 			sprintf( __( 'Choose %s', 'cominovel' ), $post_type->labels->singular_name ),
-			array( $this, 'comic_info' ),
+			array( $this, 'choose_chapter_parent' ),
 			'chapter',
 			'side'
 		);
@@ -53,7 +49,14 @@ class Cominovel_Metabox_Chapter_Data {
 		}
 	}
 
-	public function comic_info() {
+	public function choose_chapter_parent( $post ) {
+		$posts = get_posts(
+			array(
+				'post_type'      => $this->post_type,
+				'posts_per_page' => -1,
+			)
+		);
+		cominovel_core_template( 'admin/metaboxes/choose-parent', compact( 'post', 'posts' ) );
 	}
 
 	public function hide_editor_area() {
@@ -66,7 +69,16 @@ class Cominovel_Metabox_Chapter_Data {
 	}
 	public function remove_editor_for_comic( $args ) {
 		if ( empty( $_GET['data_type'] ) || $_GET['data_type'] !== 'comic' ) {
-			return $args;
+			if ( empty( $_GET['post'] ) ) {
+				return $args;
+			}
+			$post = get_post( $_GET['post'] );
+			if ( $post->post_parent <= 0 || $post->post_type !== 'chapter' ) {
+				return $args;
+			}
+			if ( get_post_type( $post->post_parent ) !== 'comic' ) {
+				return $args;
+			}
 		}
 		$supports = $args['supports'];
 		$index    = array_search( 'editor', $supports );
@@ -75,6 +87,20 @@ class Cominovel_Metabox_Chapter_Data {
 		}
 		$args['supports'] = array_values( $supports );
 		return $args;
+	}
+
+	public function save_chapter_data( $post_id, $post ) {
+		if ( $post->post_type !== 'chapter' ) {
+			return;
+		}
+		$post->post_parent = $_POST['post_parent'];
+	}
+
+	public function allow_dupplicate_slug( $pre, $slug, $post_ID, $post_status, $post_type, $post_parent ) {
+		if ( $post_type === 'chapter' ) {
+			return sanitize_title( get_the_title( $post_ID ) );
+		}
+		return $pre;
 	}
 }
 new Cominovel_Metabox_Chapter_Data();
