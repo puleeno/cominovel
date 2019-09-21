16ebcb54eb42cfecea9b1ef203a713cec0ae2fb0
Puleeno Nguyen <puleeno@gmail.com>
2019-09-22 01:51:25 +0700

    Added new template structure

--- templates/single-comic.php
+++ templates/single-comic.php
@@ -1,1 +1,16 @@
 <?php
+get_header( 'comic' );
+
+if ( have_posts() ) {
+	the_post();
+	$post = $GLOBALS['post'];
+	if ( $post->post_parent > 0 ) {
+		cominovel_template( 'comic/chapter' );
+	} else {
+		cominovel_template( 'comic/comic' );
+	}
+}
+
+get_sidebar( 'comic' );
+
+get_footer( 'comic' );
--- templates/single-novel.php
+++ templates/single-novel.php
@@ -1,2 +1,16 @@
-novel
 <?php
+get_header( 'novel' );
+
+if ( have_posts() ) {
+	the_post();
+	$post = $GLOBALS['post'];
+	if ( $post->post_parent > 0 ) {
+		cominovel_template( 'novel/chapter' );
+	} else {
+		cominovel_template( 'novel/novel' );
+	}
+}
+
+get_sidebar( 'novel' );
+
+get_footer( 'novel' );
