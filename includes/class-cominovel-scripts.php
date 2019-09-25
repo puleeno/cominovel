a899c7052e929910889fe02bf5ebcdb793a994aa
Puleeno Nguyen <puleeno@gmail.com>
2019-09-25 22:14:38 +0700

    Add register scripts method

--- includes/class-cominovel-scripts.php
+++ includes/class-cominovel-scripts.php
@@ -1,7 +1,12 @@
 <?php
 
 class Cominovel_Scripts {
+
 	public function __construct() {
+		add_action( 'wp_enqueue_scripts', array( $this, 'register_scripts' ) );
+	}
+
+	public function register_scripts() {
 	}
 }
 
