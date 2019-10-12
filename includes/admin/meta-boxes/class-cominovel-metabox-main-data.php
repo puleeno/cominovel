a48496466558b9e25c683ea1e6d3a27d8a417f42
Puleeno Nguyen <puleeno@gmail.com>
2019-10-12 19:36:00 +0700

    Save post meta for main cominovel data

--- includes/admin/meta-boxes/class-cominovel-metabox-main-data.php
+++ includes/admin/meta-boxes/class-cominovel-metabox-main-data.php
@@ -3,14 +3,15 @@ class Cominovel_Meta_Box_Comic_Data {
 
 
 	public function __construct() {
-		add_action('add_meta_boxes', array($this, 'register_meta_box'));
+		add_action( 'add_meta_boxes', array( $this, 'register_meta_box' ) );
+		add_action( 'save_post', array( $this, 'save_cominovel_data' ) );
 	}
 
-	public function register_meta_box($post_type) {
+	public function register_meta_box( $post_type ) {
 		add_meta_box(
 			'cominovel-main-info',
 			'Cominovel',
-			array($this, 'render'),
+			array( $this, 'render' ),
 			Cominovel_Post_Types::get_allowed_post_types(),
 			'advanced',
 			'high'
@@ -18,7 +19,10 @@ class Cominovel_Meta_Box_Comic_Data {
 	}
 
 	public function render() {
-		cominovel_core_template('admin/metaboxes/cominovel');
+		cominovel_core_template( 'admin/metaboxes/cominovel' );
+	}
+
+	public function save_cominovel_data( $post_id ) {
 	}
 }
 new Cominovel_Meta_Box_Comic_Data();
