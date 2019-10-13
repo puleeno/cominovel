77739ff4c5d8c9676f9be382090f4f01647f374e
Puleeno Nguyen <puleeno@gmail.com>
2019-10-13 20:42:43 +0700

    Added new template for single comic and novel

--- templates/single-comic.php
+++ templates/single-comic.php
@@ -5,12 +5,15 @@ if ( have_posts() ) {
 	the_post();
 	$post = $GLOBALS['post'];
 	if ( $post->post_parent > 0 ) {
-		cominovel_template( 'comic/chapter' );
+		$chapter = new Cominovel_Chapter( $post );
+		cominovel_template( 'comic/chapter', compact( 'chapter' ) );
 	} else {
-		cominovel_template( 'comic/comic' );
+		$comic = new Cominovel_Comic( $post );
+		cominovel_template( 'comic/comic', compact( 'comic' ) );
 	}
 }
 
-get_sidebar( 'comic' );
+do_action( 'cominovel_sidebars' );
 
+// Get theme footer
 get_footer( 'comic' );
--- templates/single-novel.php
+++ templates/single-novel.php
@@ -5,12 +5,15 @@ if ( have_posts() ) {
 	the_post();
 	$post = $GLOBALS['post'];
 	if ( $post->post_parent > 0 ) {
-		cominovel_template( 'novel/chapter' );
+		$chapter = new Cominovel_Chapter( $post );
+		cominovel_template( 'novel/chapter', compact( 'chapter' ) );
 	} else {
-		cominovel_template( 'novel/novel' );
+		$novel = new Cominovel_Novel( $post );
+		cominovel_template( 'novel/novel', compact( 'novel' ) );
 	}
 }
 
-get_sidebar( 'novel' );
+do_action( 'cominovel_sidebars' );
 
+// Get theme footer
 get_footer( 'novel' );
