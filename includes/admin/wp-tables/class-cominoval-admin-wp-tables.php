ddfebaa178a097e5417a961764d84c215b6c92be
Puleeno Nguyen <puleeno@gmail.com>
2019-10-13 22:24:31 +0700

    Add cominovel main data table

--- includes/admin/wp-tables/class-cominoval-admin-wp-tables.php
+++ includes/admin/wp-tables/class-cominoval-admin-wp-tables.php
@@ -3,6 +3,7 @@ class Cominoval_Admin_Wp_Tables {
 
 	public function __construct() {
 		require_once dirname( __FILE__ ) . '/class-cominoval-admin-wp-table-chapters.php';
+		require_once dirname( __FILE__ ) . '/class-cominoval-admin-wp-table-cominovel.php';
 	}
 }
 
