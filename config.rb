32b4e796eadbf26cbd6b5e613e3ca0741d4c0998
Puleeno Nguyen <puleeno@gmail.com>
2019-10-13 14:26:39 +0700

    Change compass config to new structure

--- config.rb
+++ config.rb
@@ -3,10 +3,10 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "assets/css"
-sass_dir = "assets/sass"
-images_dir = "assets/images"
-javascripts_dir = "assets/js"
+css_dir = "src/assets/css"
+sass_dir = "src/assets/sass"
+images_dir = "src/assets/images"
+javascripts_dir = "src/assets/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
