fba6d6017de145eb4ac87c2401ae27ce049756d0
Puleeno Nguyen <puleeno@gmail.com>
2019-10-14 02:44:30 +0700

    Prepare support user activity tracking

---
+++ includes/addons/class-cominovel-addon-user-activity.php
@@ -0,0 +1,25 @@
+<?php
+class Cominovel_Addon_User_Activity {
+	public function update_last_login() {
+	}
+
+	public function like() {
+	}
+
+	public function add_read_log() {
+	}
+
+	public function sharing() {
+	}
+
+	public function follow() {
+	}
+
+	public function upload() {
+	}
+
+	public function upgrade_account() {
+	}
+}
+
+new Cominovel_Addon_User_Activity();
