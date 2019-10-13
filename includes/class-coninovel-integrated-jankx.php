58d337b96e4b12ba26245b942d187d62197b035b
Puleeno Nguyen <puleeno@gmail.com>
2019-10-13 22:27:18 +0700

    Integrated with Jankx Framework

---
+++ includes/class-coninovel-integrated-jankx.php
@@ -0,0 +1,37 @@
+<?php
+use Jankx\SiteLayout\Layout;
+
+class Coninovel_Integrated_Jankx {
+
+	public function __construct() {
+		add_action( 'after_setup_theme', array( $this, 'integrate' ) );
+	}
+
+	public function integrate() {
+		$this->jankx_is_activated = defined( 'JANKX_FRAMEWORK_LOADED' ) || class_exists( 'Jankx' );
+		if ( ! $this->jankx_is_activated ) {
+			return;
+		}
+		add_action( 'template_redirect', array( $this, 'layout' ) );
+	}
+
+	public function layout() {
+		remove_all_actions( 'cominovel_sidebars' );
+		add_filter( 'jankx_get_layout', array( $this, 'change_layout' ) );
+	}
+
+	public function change_layout( $layout ) {
+		$template_info = Cominovel_Template::get_template_info();
+		if ( $template_info['is_cominovel'] && class_exists( Layout::class ) ) {
+			$post = $GLOBALS['post'];
+			if ( $template_info['type'] === 'chapter' ) {
+				return Layout::LAYOUT_FULL_WIDTH;
+			}
+
+			return apply_filters( 'cominovel_jankx_layout', Layout::LAYOUT_FULL_WIDTH, $template_info['type'] );
+		}
+		return $layout;
+	}
+}
+
+new Coninovel_Integrated_Jankx();
