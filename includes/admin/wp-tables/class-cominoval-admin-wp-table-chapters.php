ee5380a753c53de26c3ecc476600df92db8b2153
Puleeno Nguyen <puleeno@gmail.com>
2019-10-13 22:24:58 +0700

    Add featured image for chapter data

--- includes/admin/wp-tables/class-cominoval-admin-wp-table-chapters.php
+++ includes/admin/wp-tables/class-cominoval-admin-wp-table-chapters.php
@@ -6,6 +6,8 @@ class Cominoval_Admin_Wp_Table_Chapters {
 	public function __construct() {
 		add_action( 'current_screen', array( $this, 'get_current_screen' ) );
 		add_filter( 'the_title', array( $this, 'add_cominovel_parent_title' ), 10, 2 );
+		add_filter( 'manage_chapter_posts_columns', array( $this, 'featured_image_column' ) );
+		add_filter( 'manage_chapter_posts_custom_column', array( $this, 'feature_column' ), 10, 2 );
 	}
 
 	public function get_current_screen() {
@@ -21,6 +23,27 @@ class Cominoval_Admin_Wp_Table_Chapters {
 		$parent  = get_post( $chapter->post_parent );
 
 		return sprintf( '%s - %s', $parent->post_title, $title );
+	}
+
+	public function featured_image_column( $columns ) {
+		$replace_columns              = array_slice( $columns, 0, 1 );
+		$replace_columns['thumbnail'] = __( 'Thumbnail', 'cominovel' );
+
+		return array_merge( $replace_columns, $columns );
+	}
+
+	public function feature_column( $column, $post_id ) {
+
+		if ( 'thumbnail' === $column ) {
+			echo '<a href="' . get_edit_post_link() . '">';
+			if ( has_post_thumbnail( $post_id ) ) {
+				echo get_the_post_thumbnail( $post_id, array( 70, 70 ) );
+			} else {
+				// Image placeholder
+				cominovel_core_template( 'default/image-placeholder', [], 'admin' );
+			}
+			echo '</a>';
+		}
 	}
 }
 
