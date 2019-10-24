78c5fee4a52a86ac65c9ff3758080f86261aca85
Puleeno Nguyen <puleeno@gmail.com>
2019-10-24 21:46:09 +0700

    Added class layouts for loop start

--- templates/loop/start.php
+++ templates/loop/start.php
@@ -1,2 +1,2 @@
-<div class="layout-<?php echo $layout; ?><?php echo ! empty( $items ) ? ' r' . $items . '-items' : ''; ?>">
+<div class="cm-layouts layout-<?php echo $layout; ?><?php echo ! empty( $items ) ? ' r' . $items . '-items' : ''; ?>">
 	<div class="posts <?php echo implode( ' ', $post_type ); ?>">
