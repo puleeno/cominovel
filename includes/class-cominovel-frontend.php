327952a8474efde8b0be9b857c9342793aabd128
Puleeno Nguyen <puleeno@gmail.com>
2019-10-27 19:42:14 +0700

    Register single sidebar for comic

--- includes/class-cominovel-frontend.php
+++ includes/class-cominovel-frontend.php
@@ -4,6 +4,7 @@ class Cominovel_Frontend {
 	public static function init() {
 		add_action( 'admin_bar_menu', array( __CLASS__, 'change_edit_chapter_link_to_comic' ), 999 );
 		add_filter( 'cominovel_edit_comic_node', array( __CLASS__, 'edit_comic_node' ), 10, 2 );
+		add_action( 'widgets_init', array( __CLASS__, 'register_sidebar' ), 15 );
 	}
 
 	public function change_edit_chapter_link_to_comic( $wp_admin_bar ) {
@@ -28,6 +29,20 @@ class Cominovel_Frontend {
 			return $node;
 		}
 		return self::edit_comic_node( $node, get_post( $queried_object->post_parent ) );
+	}
+
+	public static function register_sidebar() {
+		register_sidebar(
+			array(
+				'name'          => __( 'Cominovel Single', 'cominovel' ),
+				'id'            => 'cominovel_single',
+				'description'   => __( 'Widgets in this area will be shown on comic or novel single page.', 'cominovel' ),
+				'before_widget' => '<div id="%1$s" class="widget %2$s">',
+				'after_widget'  => '</div>',
+				'before_title'  => '<h4 class="widget-title">',
+				'after_title'   => '</h4>',
+			)
+		);
 	}
 }
 
