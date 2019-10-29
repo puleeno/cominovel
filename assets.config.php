0bdb797cb756654940485337d11b1bff598f9b7b
Puleeno Nguyen <puleeno@gmail.com>
2019-10-29 21:36:15 +0700

    Create the assets.config.php file

---
+++ assets.config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+	'css' => array(
+		'main' => 'main.f36107e4.chunk.css',
+		'library' => '2.65d0fb34.chunk.css',
+	),
+	'js' => array(
+		'main' => 'main.e5363489.chunk.js',
+		'runtime' => 'runtime-main.88220290.js',
+		'library' => '2.6ec94e34.chunk.js',
+	),
+);
