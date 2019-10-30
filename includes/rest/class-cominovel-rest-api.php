be5e7ca1e0c5ef9d30cf277dd49e6f14bf3fed72
Puleeno Nguyen <puleeno@gmail.com>
2019-10-31 00:27:12 +0700

    Support cominovel rest data

--- includes/rest/class-cominovel-rest-api.php
+++ includes/rest/class-cominovel-rest-api.php
@@ -1,7 +1,18 @@
 <?php
 class Cominovel_Rest_Api {
+	const ENDPOINT_NAMESPACE  = 'cominovel/v1';
+	const ENDPOINT_COMIC_INFO = '/comic';
+	const ENDPOINT_NOVEL_INFO = '/novel';
+
+
 	public function __construct() {
+		$this->includes();
+
 		add_action( 'rest_api_init', array( $this, 'register_new_endpoint' ) );
+	}
+
+	public function includes() {
+		require_once dirname( __FILE__ ) . '/class-cominovel-rest-data.php';
 	}
 
 	public function register_new_endpoint() {
@@ -13,6 +24,8 @@ class Cominovel_Rest_Api {
 				'callback' => array( $this, 'languages' ),
 			)
 		);
+
+		new Cominovel_Rest_Data();
 	}
 
 	public function languages( WP_REST_Request $request ) {
