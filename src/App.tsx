320286b6b35a5e96493702e97590bb47f98b2440
Puleeno Nguyen <puleeno@gmail.com>
2019-10-30 12:48:56 +0700

    Support locale for cominovel app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import { ConfigProvider } from "antd";
+import { Locale } from 'antd/lib/locale-provider';
 import React from "react";
 import Cominovel from "./Cominovel";
 
+interface CominovelWindowProps {
+  languages: Locale,
+}
+
+declare global {
+  interface Window {
+    Cominovel: CominovelWindowProps,
+  }
+}
+
+window.Cominovel = window.Cominovel || { languages: {}};
+
 const App: React.FC = () => {
   return (
-    <ConfigProvider>
+    <ConfigProvider locale={window.Cominovel.languages}>
       <Cominovel />
     </ConfigProvider>
   );
