cd9ab0668983eecec13dbf5423c33e60edfaff17
Puleeno Nguyen <puleeno@gmail.com>
2019-10-30 19:40:36 +0700

    Fix REST locale response

--- includes/rest/class-cominovel-rest-api.php
+++ includes/rest/class-cominovel-rest-api.php
@@ -26,10 +26,9 @@ class Cominovel_Rest_Api {
 
 		$languages_config = require dirname( __FILE__ ) . '/configs/react-app-languages.php';
 		$json             = sprintf( 'var cominovel = {languages: %s}; window.cominovel = cominovel;', json_encode( $languages_config ) );
-		$response         = new WP_REST_Response( $json );
-		$response->header( 'Content-Type', 'text/javascript' );
 
-		return $response;
+		header( 'Content-Type: text/javascript' );
+		die( $json );
 	}
 }
 
