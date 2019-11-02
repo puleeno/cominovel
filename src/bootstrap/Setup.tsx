564a7dde3320316624675ad75bc33d8bd583e26a
Puleeno Nguyen <puleeno@gmail.com>
2019-11-03 02:17:59 +0700

    Support tricker to develop

--- src/bootstrap/Setup.tsx
+++ src/bootstrap/Setup.tsx
@@ -5,6 +5,7 @@ class Setup {
   public static bootstrap() {
     // Setup environment
     window.Cominovel = window.Cominovel || {};
+    window.Cominovel.currentID  = window.Cominovel.currentID || 14395; // This line is trick use for develop
   }
 }
 
