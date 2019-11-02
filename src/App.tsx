063d43e2b9fa6f8639ca7684b3bb969c951e529e
Puleeno Nguyen <puleeno@gmail.com>
2019-11-02 21:31:34 +0700

    Fix coding standard

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { IntlProvider } from "react-intl";
 import Cominovel from "./Cominovel";
-import { ICominovelProps } from './interfaces/CominovelProps';
+import { ICominovelProps } from "./interfaces/CominovelProps";
 
 declare global {
   // tslint:disable-next-line: interface-name
