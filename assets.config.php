bdd59dbd22db5856efac4b999c2d8e6fba7cbe31
Puleeno Nguyen <puleeno@gmail.com>
2019-11-05 20:53:35 +0700

    Auto generate asset.configs.php

--- assets.config.php
+++ assets.config.php
@@ -1,12 +1,14 @@
 <?php
-return array(
-	'css' => array(
-		'main'    => 'main.f36107e4.chunk.css',
-		'library' => '2.802837ff.chunk.css',
-	),
-	'js'  => array(
-		'runtime' => 'runtime-main.77c67387.js',
-		'main'    => 'main.f1e9f5ec.chunk.js',
-		'library' => '2.039a3380.chunk.js',
-	),
+array (
+  'css' => 
+  array (
+    'library' => '2.802837ff.chunk.css',
+    'main' => 'main.f36107e4.chunk.css',
+  ),
+  'js' => 
+  array (
+    'library' => '2.039a3380.chunk.js',
+    'main' => 'main.f1e9f5ec.chunk.js',
+    'runtime' => 'runtime-main.77c67387.js',
+  ),
 );
