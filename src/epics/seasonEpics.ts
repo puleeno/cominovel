fd57e24c9f3ae977e103b10c16742426c6a47a9b
Puleeno Nguyen <puleeno@gmail.com>
2019-11-06 18:45:16 +0700

    Fix infinity loop

--- src/epics/seasonEpics.ts
+++ src/epics/seasonEpics.ts
@@ -1,6 +1,18 @@
-import { Action } from "redux";
-import { Epic } from "redux-observable";
+import { Action, AnyAction } from "redux";
+import { Epic, ofType } from "redux-observable";
+import { from } from "rxjs";
+import { ajax } from "rxjs/ajax";
+import { map, mergeMap } from "rxjs/operators";
+import { fetchSeasons, fetchSeasonsFullFilled } from "../actions";
 
 export const getCominovelSeasonsViaAjax:
   Epic<Action<any>, Action<any>, void> = (action$) => action$.pipe(
+    ofType(fetchSeasons),
+    mergeMap(
+      (action: AnyAction) => from(
+        ajax.getJSON(`http://loveofboys.io/wp-json/cominovel/v1/comic/${action.payload}`),
+      ).pipe(
+        map((response: any) => fetchSeasonsFullFilled(response)),
+      ),
+    ),
   );
