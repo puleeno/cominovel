e72f8c60b18c3731130fb689e948470363b1c92d
Puleeno Nguyen <puleeno@gmail.com>
2019-11-08 00:04:12 +0700

    Change the structure of Cominovel prepare load data

--- src/Cominovel.tsx
+++ src/Cominovel.tsx
@@ -14,10 +14,12 @@ import { IRootState } from "./reducers";
 const { TabPane } = Tabs;
 type IProps = ReturnType<typeof mapDispatchToProps> & ReturnType<typeof mapStateToProps>;
 interface IState {
+  chapterLoaded: boolean;
   seasonLoaded: boolean;
 }
 
 const initState: IState = {
+  chapterLoaded: false,
   seasonLoaded: false,
 };
 
@@ -36,19 +38,62 @@ class Cominovel extends Component<IProps, IState> {
   }
 
   public handleModeChange = (tab: any) => {
-    if (tab === 'season') {
+    if (tab === "season") {
       this.setState({
         seasonLoaded: true,
       });
     }
+    if (tab === "chapter") {
+      this.setState({
+        chapterLoaded: true,
+      });
+    }
   }
 
-  public renderSeasons = () => {
-    if (!this.state.seasonLoaded) {
-      return;
-    }
-    return (<Seasons />);
-  }
+  public renderBasicInfos = () => (
+    <TabPane
+      tab="Basic Info"
+      key="basic"
+    >
+      <BasicInfo />
+    </TabPane>
+  )
+
+  public renderChapters = () => (
+    <TabPane
+      tab="Chapters"
+      key="chapter"
+    >
+      {!this.state.chapterLoaded ? (<Chapters />) : null}
+    </TabPane>
+  )
+
+  public renderSeasons = () => (
+    <TabPane
+      tab="Seasons"
+      key="season"
+    >
+      {!this.state.seasonLoaded ? (<Seasons />) : null}
+    </TabPane>
+  )
+
+  public renderAdvanced = () => (
+    <TabPane
+      tab="Advanced"
+      key="advanced"
+    >
+      <Advanced />
+    </TabPane>
+  )
+
+  public renderComposer = () => (
+    <TabPane
+      tab="Composer"
+      key="composer"
+    >
+      <Composer />
+    </TabPane>
+  )
 
   public render() {
     if (this.props.isLoaded === null) {
@@ -63,41 +108,11 @@ class Cominovel extends Component<IProps, IState> {
         type="card"
         onChange={this.handleModeChange}
       >
-          <TabPane
-          tab="Basic Info"
-          key="basic"
-          >
-            <BasicInfo />
-          </TabPane>
-
-          <TabPane
-            tab="Chapters"
-            key="chapter"
-          >
-            <Chapters />
-          </TabPane>
-
-          <TabPane
-            tab="Seasons"
-            key="season"
-
-          >
-            {this.renderSeasons()}
-          </TabPane>
-
-          <TabPane
-            tab="Advanced"
-            key="advanced"
-          >
-            <Advanced />
-          </TabPane>
-
-          <TabPane
-            tab="Composer"
-            key="composer"
-          >
-            <Composer />
-          </TabPane>
+        {this.renderBasicInfos()}
+        {this.renderChapters()}
+        {this.renderSeasons()}
+        {this.renderAdvanced()}
+        {this.renderComposer()}
       </Tabs>
     );
   }
@@ -108,8 +123,7 @@ const mapStateToProps = (state: IRootState) => ({
   seasons: state.seasons,
 });
 
-const mapDispatchToProps = (dispatch: Dispatch<AnyAction>) =>
-bindActionCreators({
+const mapDispatchToProps = (dispatch: Dispatch<AnyAction>) => bindActionCreators({
   fetchCominovel,
 }, dispatch);
 
